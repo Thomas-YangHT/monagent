0eb9751a76357b95ab1c6a9658ab5f3bbb9f2e75
Thomas yang <168447636@qq.com>
2019-12-27 23:06:16 +0800

    1

---
+++ getIpPort.sh
@@ -0,0 +1,16 @@
+#从给定的任意服务名，取得相对应IP和PORT
+[ "$1"='' ] &&  NAME='monagent' ||  NAME=$1
+podIp=`kubectl get pods --all-namespaces -o wide|grep $NAME|awk '{print $7}'`
+a=(`kubectl get svc --all-namespaces -o wide|grep $NAME|awk '{print $4":"$6}'|sed -e 's/:/ /g' -e 's#/TCP##'`)
+svc=${a[0]}
+svcPort=${a[1]}
+nodePort=${a[2]}
+tee <<EOF
+{
+  "podIp:" "$podIp"
+  "svc:" "$svc"
+  "svcPort:" "$svcPort"
+  "nodePort:" "$nodePort"
+}
+EOF
+
