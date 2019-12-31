4680b397310584ab0c28a5d68fcb8461b70d62ff
Thomas yang <168447636@qq.com>
2019-12-31 11:34:25 +0800

    1

--- monagent/monagent.py
+++ monagent/monagent.py
@@ -14,12 +14,12 @@ sys.setdefaultencoding('utf-8')
 HOST = ''
 PORT = 18000
 
-text_content = '''
-<!DOCTYPE html>
-<html><!--STATUS OK-->
+text_content = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>WOW</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <p>Upload Control Messages</p>
 <form name="input" action="/" method="post">
