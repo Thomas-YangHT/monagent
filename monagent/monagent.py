50216c4357f95edff75ecbb32b9c24ab142e8392
Thomas yang <168447636@qq.com>
2019-12-31 11:48:55 +0800

    Update monagent.py

--- monagent/monagent.py
+++ monagent/monagent.py
@@ -14,17 +14,17 @@ sys.setdefaultencoding('utf-8')
 HOST = ''
 PORT = 18000
 
-text_content = '''Status：OK - 200
-Date：Sun, 01 Jun 2008 12:35:47 GMT
-Server：Apache/2.0.61 (Unix)
-Last-Modified：Sun, 01 Jun 2008 12:35:30 GMT
-Accept-Ranges：bytes
-Content-Length：18616
-Cache-Control：max-age=120
-Expires：Sun, 01 Jun 2008 12:37:47 GMT
-Content-Type：application/xml
-Age：2
-Connection：close
+text_content = '''Status: OK - 200
+Date: Sun, 01 Jun 2008 12:35:47 GMT
+Server: Apache/2.0.61 (Unix)
+Last-Modified:Sun, 01 Jun 2008 12:35:30 GMT
+Accept-Ranges:bytes
+Content-Length:18616
+Cache-Control:max-age=120
+Expires:Sun, 01 Jun 2008 12:37:47 GMT
+Content-Type:application/xml
+Age:2
+Connection:close
 <!doctype html> 
 <html>
 <head>
