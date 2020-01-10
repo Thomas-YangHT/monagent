6f11c5e0867ffeab4976344f5ce43e7315b2b265
Thomas yang <168447636@qq.com>
2020-01-10 16:46:12 +0800

    Update instmon.sh

--- monclient/instmon.sh
+++ monclient/instmon.sh
@@ -5,11 +5,11 @@ ETH=`ip a|grep $NETWORK|sed 's/noprefixroute//g'|awk '{print $7}'|grep -v "lo:"|
 KEYname=xwx@k8host
 USER=`whoami`
 HOMEDIR=${HOME}/monagent.client
-DownloadURL=192.168.10.92
+DownloadURL=http://192.168.10.92/monagent.client
 crontab -l|grep -vP "uploadmon.py|nettrafic" >/var/spool/cron/${USER}
 
 echo "*/5 * * * * sleep 5;/usr/bin/python $HOMEDIR/uploadmon.py upmoninfo >$HOMEDIR/upmoninfo.log 2>&1 "  >>/var/spool/cron/${USER}
-echo "*/5 * * * * /bin/bash $HOMEDIR/nettrafic.sh $ETH >>$HOMEDIR/nettrafic.log 2>&1"  >>/var/spool/cron/${USER}
+#echo "*/5 * * * * /bin/bash $HOMEDIR/nettrafic.sh $ETH >>$HOMEDIR/nettrafic.log 2>&1"  >>/var/spool/cron/${USER}
 echo "*/5 * * * * sleep 5;/usr/bin/python $HOMEDIR/uploadmon.py upportinfo >$HOMEDIR/upportinfo.log 2>&1"  >>/var/spool/cron/${USER}
 echo "*/5 * * * * /usr/bin/python $HOMEDIR/fileUpdate.py uploadmon.py >$HOMEDIR/fileupdate.log 2>&1"  >>/var/spool/cron/${USER}
 echo "*/5 * * * * /usr/bin/python $HOMEDIR/fileUpdate.py collexec.sh >$HOMEDIR/fileupdate.log 2>&1"  >>/var/spool/cron/${USER}
@@ -21,9 +21,8 @@ echo "*/5 * * * * /usr/bin/python $HOMEDIR/uploadmon.py upbaseinfo >$HOMEDIR/upb
 cd $HOMEDIR && \
 curl -O  $DownloadURL/fileUpdate.py  &&  \
 python fileUpdate.py uploadmon.py && \
-python $HOMEDIR/fileUpdate.py collexec.sh
-python $HOMEDIR/fileUpdate.py collfunc
-python $HOMEDIR/fileUpdate.py collconf
- 
+python $HOMEDIR/fileUpdate.py collexec.sh && \
+python $HOMEDIR/fileUpdate.py collfunc && \
+python $HOMEDIR/fileUpdate.py collconf && \
 /usr/bin/python $HOMEDIR/uploadmon.py upbaseinfo 
 [ "`grep $KEYname  ~/.ssh/authorized_keys`" ] && echo "Already install server key" || /usr/bin/python $HOMEDIR/uploadmon.py downkey
