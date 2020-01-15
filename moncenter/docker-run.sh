65f321ce085c6815fa4498c838b8e857261d4612
Thomas yang <168447636@qq.com>
2020-01-15 09:27:28 +0800

    1

--- moncenter/docker-run.sh
+++ moncenter/docker-run.sh
@@ -1,5 +1,3 @@
-#Modify $MYHOME  with your mysql data directory
-MYHOME=/root/cmp_mysql/mysql/info/
 DNS=223.5.5.5
 
 docker rm -f moncenter
@@ -7,8 +5,5 @@ docker run --name moncenter \
 --restart=always \
 --dns=$DNS \
 -e TZ='Asia/Shanghai' \
--v $MYHOME:/info/  \
 -v $PWD/rootcron:/etc/crontabs/root \
 -d moncenter sh START.sh
-
-#/export/opt/cmp_mysql2/mysql/info/  是mysql容器的目录，同dwloadmon.py里的MYHOME配置
