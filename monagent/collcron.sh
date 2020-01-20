89054383ba0a3e0f466f3377bedb616587f36ec3
Thomas yang <168447636@qq.com>
2020-01-20 16:12:58 +0800

    Update collcron.sh

--- monagent/collcron.sh
+++ monagent/collcron.sh
@@ -1,11 +1,12 @@
-/usr/bin/python /root/monagent.client/fileUpdate.py uploadmon.py > /root/monagent.client/fileupdate.log 2>upload.err && \
-/usr/bin/python /root/monagent.client/fileUpdate.py collexec.sh  >>/root/monagent.client/fileupdate.log 2>>upload.err && \
-/usr/bin/python /root/monagent.client/fileUpdate.py collfunc     >>/root/monagent.client/fileupdate.log 2>>upload.err && \
-/usr/bin/python /root/monagent.client/fileUpdate.py collconf     >>/root/monagent.client/fileupdate.log 2>>upload.err && \
-/usr/bin/python /root/monagent.client/fileUpdate.py collcron.sh  >>/root/monagent.client/fileupdate.log 2>>upload.err && \
-/usr/bin/python /root/monagent.client/uploadmon.py upmoninfo  >/root/monagent.client/upmoninfo.log  2>>upload.err 
-/usr/bin/python /root/monagent.client/uploadmon.py upbaseinfo >/root/monagent.client/upbaseinfo.log 2>>upload.err
-/usr/bin/python /root/monagent.client/uploadmon.py upportinfo >/root/monagent.client/upportinfo.log 2>>upload.err
-/usr/bin/python /root/monagent.client/uploadmon.py upwebinfo  >/root/monagent.client/upwebinfo.log  2>>upload.err
-#/usr/bin/python /root/monagent.client/uploadmon.py upbakinfo  >/root/monagent.client/upbakinfo.log  2>>upload.err
-/usr/bin/python /root/monagent.client/uploadmon.py uperrinfo  >/root/monagent.client/uperrinfo.log  2>>upload.err
+cd $HOME/monagent.client
+/usr/bin/python ./fileUpdate.py uploadmon.py > ./fileupdate.log 2>upload.err && \
+/usr/bin/python ./fileUpdate.py collexec.sh  >>./fileupdate.log 2>>upload.err && \
+/usr/bin/python ./fileUpdate.py collfunc     >>./fileupdate.log 2>>upload.err && \
+/usr/bin/python ./fileUpdate.py collconf     >>./fileupdate.log 2>>upload.err && \
+/usr/bin/python ./fileUpdate.py collcron.sh  >>./fileupdate.log 2>>upload.err && \
+/usr/bin/python ./uploadmon.py upmoninfo  >./upmoninfo.log  2>>upload.err 
+/usr/bin/python ./uploadmon.py upbaseinfo >./upbaseinfo.log 2>>upload.err
+/usr/bin/python ./uploadmon.py upportinfo >./upportinfo.log 2>>upload.err
+/usr/bin/python ./uploadmon.py upwebinfo  >./upwebinfo.log  2>>upload.err
+#/usr/bin/python ./uploadmon.py upbakinfo  >./upbakinfo.log  2>>upload.err
+/usr/bin/python ./uploadmon.py uperrinfo  >./uperrinfo.log  2>>upload.err
