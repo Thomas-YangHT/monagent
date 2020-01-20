20f2d2d6311dcc4f25997ca385d9762454b5832f
Thomas yang <168447636@qq.com>
2020-01-20 13:29:39 +0800

    Update collcron.sh

--- monagent/collcron.sh
+++ monagent/collcron.sh
@@ -6,6 +6,6 @@
 /usr/bin/python /root/monagent.client/uploadmon.py upmoninfo  >/root/monagent.client/upmoninfo.log  2>>err 
 /usr/bin/python /root/monagent.client/uploadmon.py upbaseinfo >/root/monagent.client/upbaseinfo.log 2>>err
 /usr/bin/python /root/monagent.client/uploadmon.py upportinfo >/root/monagent.client/upportinfo.log 2>>err
-#/usr/bin/python /root/monagent.client/uploadmon.py upwebinfo  >/root/monagent.client/upwebinfo.log  2>>err
-#/usr/bin/python /root/monagent.client/uploadmon.py uperrinfo  >/root/monagent.client/uperrinfo.log  2>>err
-#/usr/bin/python /root/monagent.client/uploadmon.py upbakinfo  >/root/monagent.client/upbakinfo.log  2>>err
+/usr/bin/python /root/monagent.client/uploadmon.py upwebinfo  >/root/monagent.client/upwebinfo.log  2>>err
+/usr/bin/python /root/monagent.client/uploadmon.py upbakinfo  >/root/monagent.client/upbakinfo.log  2>>err
+/usr/bin/python /root/monagent.client/uploadmon.py uperrinfo  >/root/monagent.client/uperrinfo.log  2>>err
